734cc7bde677c61cf69bf956fa063cf92bd6f590
josilob <bjosilo@gmail.com>
2021-02-15 14:50:22 -0500

    form error handling ui, class and conditional render

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { Field, formValues, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-	renderInput({ input, label, meta }) {
+	renderError({ error, touched }) {
+		if (touched && error) {
+			return (
+				<div className='ui error message'>
+					<div className='header'>{error}</div>
+				</div>
+			);
+		}
+	}
+	renderInput = ({ input, label, meta }) => {
+		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
 		return (
-			<div className='field'>
+			<div className={className}>
 				<label>{label}</label>
-				<input {...input} />
-				<div>{meta.error}</div>
+				<input {...input} autoComplete='off' />
+				{this.renderError(meta)}
 			</div>
 		);
-	}
+	};
 
 	onSubmit(formValues) {
 		console.log(formValues);
@@ -18,7 +28,9 @@ class StreamCreate extends React.Component {
 
 	render() {
 		return (
-			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className='form ui'>
+			<form
+				onSubmit={this.props.handleSubmit(this.onSubmit)}
+				className='form ui error'>
 				<Field name='title' component={this.renderInput} label='Enter Title' />
 				<Field
 					name='description'
