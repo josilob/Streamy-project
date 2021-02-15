a8b9968ecc2f64b1396dc447ae17205c60851eae
josilob <bjosilo@gmail.com>
2021-02-15 14:26:52 -0500

    form input fields added, props passed

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+	renderInput({ input, label }) {
+		return (
+			<div className='field'>
+				<label>{label}</label>
+				<input {...input} />
+			</div>
+		);
+	}
 	render() {
 		return (
-			<form>
-				<Field name='title' />
-				<Field name='description' />
+			<form className='form ui'>
+				<Field name='title' component={this.renderInput} label='Enter Title' />
+				<Field
+					name='description'
+					component={this.renderInput}
+					label='Enter Description'
+				/>
 			</form>
 		);
 	}
