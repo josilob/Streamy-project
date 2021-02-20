00b69c82e3097a942fe4787231925e5b1e3e7590
josilob <bjosilo@gmail.com>
2021-02-20 00:08:34 -0500

    PUT replaced with PATCH request to fix the form edit submition

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
 import StreamForm from './StreamForm';
@@ -9,17 +10,22 @@ class StreamEdit extends React.Component {
 	}
 
 	onSubmit = (formValues) => {
-		console.log(formValues);
+		this.props.editStream(this.props.match.params.id, formValues);
 	};
 
 	render() {
 		if (!this.props.stream) {
 			return <div>Loading...</div>;
 		}
+
+		// lodash in return below picks keys(properties) of object in front of it
 		return (
 			<div>
 				<h3>Edit a Stream</h3>
-				<StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+				<StreamForm
+					initialValues={_.pick(this.props.stream, 'title', 'description')}
+					onSubmit={this.onSubmit}
+				/>
 			</div>
 		);
 	}
